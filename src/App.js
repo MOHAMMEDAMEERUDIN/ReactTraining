22f3ee81fa033794cbd41ae0442cd669c86f9fb5
mohammed.ameerudin <mohammed.ameerudin@hmhco.com>
2021-01-25 19:37:16 +0530

    API using Class vs Functional components

--- src/App.js
+++ src/App.js
@@ -21,19 +21,19 @@ export default App;
 
 
 
-//class api call
+//******************************** Class Component api call ******************************
 /*class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      products: []
+      pokes: []
     }
   }
 
   componentDidMount = () => {
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=200").then(json => {
       console.log(json);
-        this.setState({ products: json.data.results });
+        this.setState({ pokes: json.data.results });
     });
   }
 
@@ -41,44 +41,73 @@ export default App;
     return (
       <>
         <Header />
-        {this.state.products.map(function (product, index) {
-              return <p>{product.name}</p>
+        <strong>Class Component with componentDidMount() using Axios API call</strong>
+        <table style={{width:'100%'}}>
+          <tr style={{border:'2px solid red'}}>
+            <th>SI No</th>
+            <th>Name</th>
+            <th>Url</th>
+          </tr>
+          {this.state.pokes.map(function (poke, index) {
+              return <>                  
+                <tr style={{border:'1px solid orange'}}>
+                  <td>{index+1}</td>
+                  <td>{poke.name}</td>
+                  <td><a href={poke.url}>{poke.url}</a></td>
+                </tr>
+              </>
             })}
+        </table>
       </>
     )
   }
 }
 
-export default App;
+export default App;*/
 
 
-//function api call
-const App =() =>{
-  var [datas , setDatas] = useState();
-  useEffect(()=>{
-   
-      fetch(
-        'http://10.5.5.104:8080/hmhstudents/data/shopiee/getAllProducts',
-        {
-          method : 'GET',
-          mode: 'no-cors'
-        }
-      )
-      .then(res => res.json())
-      .then(response => {
-        console.log(response);
-        setDatas(response);
-      })
+//******************************** Functional Component api call ******************************
+
+/*const App = () => {
+  const [pokes, setPokes] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=200");
+      res.json()
+        .then(res => {
+          console.log(res.results.map(function (poke, index) {
+            return <p>{poke.name}</p>
+          }));
+          setPokes(res.results)
+        })
+    }
+
+    fetchData();
   });
 
   return (
     <>
-      <Header />
-      {datas.prods.map(function (product, index) {
-        return <Result pId={product.productId} mrpPrice={product.mrpPrice} offerPrice={product.offerPrice} desc={product.desc} imageUrl={product.imageUrl} />
-      })}
-    </>
-  );
-}
+        <Header />
+        <strong>Functional Component with useEffect() using async fetch api call</strong>
+        <table style={{width:'100%'}}>
+          <tr style={{border:'2px solid red'}}>
+            <th>SI No</th>
+            <th>Name</th>
+            <th>Url</th>
+          </tr>
+          {pokes.map(function (poke, index) {
+              return <>                  
+                <tr style={{border:'1px solid blue'}}>
+                  <td>{index+1}</td>
+                  <td>{poke.name}</td>
+                  <td><a href={poke.url}>{poke.url}</a></td>
+                </tr>
+              </>
+            })}
+        </table>
 
+      </>
+  );
+};
 export default App;*/
