19e4ff136a1c9cb23e374e29ea2c5d9d5a435df5
mohammed.ameerudin <mohammed.ameerudin@hmhco.com>
2021-01-25 12:23:09 +0530

    Data changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {useState , useEffect} from 'react';
 import ReactDOM from 'react-dom';
-import products from './datas.json';
+import datas from './datas.json';
 import Header from './Header.js';
 import Result from './Result.js';
 
 
 const App =() =>{
+  useEffect(()=>{
+      fetch(
+        ''
+      )
+  });
+
   return (
     <>
       <Header />
-      {products.map(function (product, index) {
+      {datas.prods.map(function (product, index) {
         return <Result pId={product.productId} mrpPrice={product.mrpPrice} offerPrice={product.offerPrice} desc={product.desc} imageUrl={product.imageUrl} />
       })}
     </>
