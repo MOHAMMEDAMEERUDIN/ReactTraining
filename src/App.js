14dcb1264fb47046286c7888d3300dbca7fdf271
mohammed.ameerudin <mohammed.ameerudin@hmhco.com>
2021-01-25 18:09:33 +0530

    Sample Axios impl

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React, {useState , useEffect} from 'react';
+import React, {Component, useState , useEffect} from 'react';
+
 import ReactDOM from 'react-dom';
 import datas from './datas.json';
 import Header from './Header.js';
 import Result from './Result.js';
-
+import axios from 'axios';
 
 const App =() =>{
-  //var [datas , setDatas] = useState();
-  // useEffect(()=>{
+  var [datas , setDatas] = useState();
+  useEffect(()=>{
    
-  //     fetch(
-  //       'http://10.5.5.104:8080/hmhstudents/data/shopiee/getAllProducts',
-  //       {
-  //         method : 'GET',
-  //         mode: 'no-cors'
-  //       }
-  //     )
-  //     .then(res => res.json())
-  //     .then(response => {
-  //       console.log(response);
-  //       setDatas(response);
-  //     })
-  // });
+      fetch(
+        'http://10.5.5.104:8080/hmhstudents/data/shopiee/getAllProducts',
+        {
+          method : 'GET',
+          mode: 'no-cors'
+        }
+      )
+      .then(res => res.json())
+      .then(response => {
+        console.log(response);
+        setDatas(response);
+      })
+  });
 
   return (
     <>
@@ -34,4 +35,36 @@ const App =() =>{
 }
 
 export default App;
+
+
+
+
+/*class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      products: []
+    }
+  }
+
+  componentDidMount = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=200").then(json => {
+      console.log(json);
+        this.setState({ products: json.data.results });
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        {this.state.products.map(function (product, index) {
+              return <p>{product.name}</p>
+            })}
+      </>
+    )
+  }
+}
+
+export default App;*/
 
